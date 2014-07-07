c6c58d4989d91a147786c50e240cd395ff80b4f1
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2014-07-07 17:19:13 -0300

    Provide a ScreenOrientationDispatcherHost getter.
    SSIA. We need it in order to be able to set ScreenOrientationProvider
    from Xwalk.

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -52,6 +52,7 @@ class RenderFrameHost;
 class RenderProcessHost;
 class RenderViewHost;
 class RenderWidgetHostView;
+class ScreenOrientationDispatcherHost;
 class SiteInstance;
 class WebContentsDelegate;
 struct CustomContextMenuContext;
@@ -145,6 +146,8 @@ class WebContents : public PageNavigator,
   virtual ~WebContents() {}
 
   // Intrinsic tab state -------------------------------------------------------
+
+  virtual ScreenOrientationDispatcherHost* GetScreenOrientationDispatcherHost() = 0;
 
   // Gets/Sets the delegate.
   virtual WebContentsDelegate* GetDelegate() = 0;
