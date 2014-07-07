908da5fccc162bb5858dade95738d9646d244855
guangzhen <guangzhen.li@intel.com>
2014-07-07 14:11:04 -0300

    [Android] Add a new listener to listen the first rendered frame To support the feature of launch screen. Added a new listener FirstRenderedFrameListener. This listener will be triggered when the first visible content has been updated to the screen.
    SPEC: https://docs.google.com/a/intel.com/document/d/17PuNuHRTQuREUpaCvj-eEx7uYi2avd-VW-oaMXMpvwo/edit?pli=1#
    Design Doc: https://docs.google.com/a/intel.com/document/d/1YLajlZC7CkBOtEvzD6p6yeBr0HrPsaIGX8Eqhr3oTF4/edit#heading=h.mfsu3bk9566e
    Bug: XWALK-637
    
    Reworked for M37

--- content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewRenderView.java
@@ -36,6 +36,17 @@ public class ContentViewRenderView extends FrameLayout {
     protected ContentViewCore mContentViewCore;
 
     private ContentReadbackHandler mContentReadbackHandler;
+    // The listener which will be triggered when below two conditions become valid.
+    // 1. The view has been initialized and ready to draw content to the screen.
+    // 2. The compositor finished compositing and the OpenGL buffers has been swapped.
+    //    Which means the view has been updated with visually non-empty content.
+    // This listener will be triggered only once after registered.
+    private FirstRenderedFrameListener mFirstRenderedFrameListener;
+    private boolean mFirstFrameReceived;
+
+    public interface FirstRenderedFrameListener{
+        public void onFirstFrameReceived();
+    }
 
     /**
      * Constructing the SurfaceView early sends surface created notifications
@@ -229,6 +240,13 @@ public class ContentViewRenderView extends FrameLayout {
         return new SurfaceView(context);
     }
 
+    public void registerFirstRenderedFrameListener(FirstRenderedFrameListener listener) {
+        mFirstRenderedFrameListener = listener;
+        if (mFirstFrameReceived && mFirstRenderedFrameListener != null) {
+            mFirstRenderedFrameListener.onFirstFrameReceived();
+        }
+    }
+
     /**
      * @return whether the surface view is initialized and ready to render.
      */
@@ -260,6 +278,14 @@ public class ContentViewRenderView extends FrameLayout {
 
     @CalledByNative
     private void onSwapBuffersCompleted() {
+        if (!mFirstFrameReceived && mContentViewCore != null &&
+                mContentViewCore.isReady()) {
+            mFirstFrameReceived = true;
+            if (mFirstRenderedFrameListener != null) {
+                mFirstRenderedFrameListener.onFirstFrameReceived();
+            }
+        }
+
         if (mSurfaceView.getBackground() != null) {
             post(new Runnable() {
                 @Override public void run() {
